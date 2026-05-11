93b8660e64ec8c7a05c35252474c3e0a62f48563
JopsonDev <jopsonmason@outlook.com>
2026-05-11 13:32:11 -0400

    set a few attributes and called them

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -2,6 +2,20 @@ package com.pluralsight;
 
 public class Main {
     public static void main(String[] args) {
+        Car car = new Car();
+        HoverCraft hoverCraft = new HoverCraft();
+        SemiTruck semiTruck = new SemiTruck();
+        Moped moped = new Moped();
+
+        car.setColor("Red");
+        hoverCraft.setTopSpeed(100);
+        semiTruck.setNumberOfPassengers(2);
+        moped.setModel("v12");
+
+        System.out.println(car.getColor());
+        System.out.println(hoverCraft.getTopSpeed());
+        System.out.println(semiTruck.getNumberOfPassengers());
+        System.out.println(moped.getModel());
 
     }
 }
--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -8,6 +8,55 @@ public class Vehicle {
     private int numberOfPassengers;
     private int cargoCapacity;
 
+
+    public int getCargoCapacity() {
+        return cargoCapacity;
+    }
+
+    public void setCargoCapacity(int cargoCapacity) {
+        this.cargoCapacity = cargoCapacity;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getFuelCapacity() {
+        return fuelCapacity;
+    }
+
+    public void setFuelCapacity(int fuelCapacity) {
+        this.fuelCapacity = fuelCapacity;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getNumberOfPassengers() {
+        return numberOfPassengers;
+    }
+
+    public void setNumberOfPassengers(int numberOfPassengers) {
+        this.numberOfPassengers = numberOfPassengers;
+    }
+
+    public int getTopSpeed() {
+        return topSpeed;
+    }
+
+    public void setTopSpeed(int topSpeed) {
+        this.topSpeed = topSpeed;
+    }
+
     public void startEngine(){}
 
     public void stopEngine(){}
